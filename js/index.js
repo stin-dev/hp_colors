0a92ef9b6a662d07bed29a4d6220f93f9ad0ca02
Yosuke Hiraoka <yoh.s.c.sep10@gmail.com>
2019-05-05 22:50:44 +0900

    メニューバーのクリックでリンク一覧が表示されるように修正

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-$(function() {
-    
+$(function () {
+    $("#menu-bars").click(function () {
+        var list = $(".header-link-list");
+
+        if (list.hasClass("is-showing")) {
+            list.removeClass("is-showing");
+            list.hide();
+            $("#clear-float").removeClass("clear-float");
+        } else {
+            list.addClass("is-showing");
+            $("#clear-float").addClass("clear-float");
+            list.show();
+        }
+    });
 });
